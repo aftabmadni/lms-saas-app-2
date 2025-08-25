e7a31b7e8d60b58b6ee0f89b8d7554e1c63251bc
Aftab Madni <aftabmadni7118@gmail.com>
2025-08-26 00:28:39 +0530

    Fixed Profile Not Rendering

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,22 +1,7 @@
-"use client";
-
-import { SignIn, useAuth } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    const { isSignedIn } = useAuth();
-    const router = useRouter();
-
-    useEffect(() => {
-        if (isSignedIn) {
-            router.push("/"); // redirect home if already signed in
-        }
-    }, [isSignedIn, router]);
-
-    return (
-        <main className="flex items-center justify-center min-h-screen">
-            <SignIn forceRedirectUrl="/" />
-        </main>
-    );
+    return <main className="flex items-center justify-center">
+        <SignIn />
+    </main>
 }
